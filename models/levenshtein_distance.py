33ba8cf30a25c989f100a68fdddc47e94fea614a
jakub <jakubrogulski01@gmail.com>
2024-01-25 20:23:44 +0100

    add recursive levenshtein distance algorithm

---
+++ models/levenshtein_distance.py
@@ -0,0 +1,20 @@
+def levenshtein_distance(word1, word2):
+    '''
+    Implementation based on pseudo code
+    '''
+    if len(word1) == 0:
+        return len(word2)
+    if len(word2) == 0:
+        return len(word1)
+        
+    indicator = 1 if word1[0] != word2[0] else 0
+    # Deletion
+    levd1 = levdist(word1[1:], word2) 
+    # Insertion
+    levd2 = levdist(word1, word2[1:]) 
+    # Substitution
+    levd3 = levdist(word1[1:], word2[1:]) 
+
+    return indicator + min(levd1, levd2, levd3)
+
+levdist(word1, word2)
